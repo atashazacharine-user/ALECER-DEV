4bff5d2dccf4f3981b5c0aef45f83aba92e2253f
atashazacharine-user <atashazacharine@gmail.com>
2025-12-01 08:55:16 +0800

    Added intial insert after

--- DSA/linkedlist.js
+++ DSA/linkedlist.js
@@ -24,31 +24,59 @@ class LinkedList{
         while(current){
             output += current.value + " ";
             current = current.next;
+        }
+     return output;
     }
-    return output;
-}
 
-insert_at_end(element){
-    const node = new Node(element);
+     insert_at_end(element){
+     const node = new Node(element);
 
     if(!this.head){
         this.head = node;
         return;
-    }
-    let current = this.head;
-    while(current.next){
+     }
+     let current = this.head;
+     while(current.next){
         current = current.next;
+      }
+
+     current.next = node;
+
+    }
+    insert_after(taget, element){
+        let current = this.head;
+        while(current){
+            if( current.value === taget){
+                const node = new Node(element);
+                node.next = current.next;
+                current.next = node;
+                return;
+            }
+            current = current.next;
+        }
+
+        console.log("Target not found");
     }
 
-    current.next = node;
+    search(element){
+        let current = this.head;
+        while(current){
+            if(current.value === element) 
+            //return current;
+            return "Element found"
+            current = current.next;
+            
+        }
+        return "Element not found";
 
- }
-
+     }
 }
 
 //
 let list1 = new LinkedList();
-list1.insert_at_end(4);
-list1.insert_at_end(2);
+    list1.insert_at_end(4);
 list1.insert_at_end(3);
+list1.insert_at_end(5);
+list1.insert_after(5,2);
 console.log(list1.traverse());
+console.log(list1.search(4));
